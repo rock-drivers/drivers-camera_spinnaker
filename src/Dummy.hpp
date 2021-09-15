9f6a2003842463b22016bc06bacb1fa94e748f74
jhidalgocarrio <havyhidago@gmail.com>
2021-09-15 19:22:05 +0200

    Initial commit

---
+++ src/Dummy.hpp
@@ -0,0 +1,18 @@
+#ifndef _DUMMYPROJECT_DUMMY_HPP_
+#define _DUMMYPROJECT_DUMMY_HPP_
+
+namespace camera_spinnaker
+{
+    class DummyClass
+    {
+        public: 
+            /**
+             * Print a welcome to stdout
+             * \return nothing
+             */
+            void welcome();
+    };
+
+} // end namespace camera_spinnaker
+
+#endif // _DUMMYPROJECT_DUMMY_HPP_
